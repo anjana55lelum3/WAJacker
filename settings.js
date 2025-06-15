002f48e57bdf2ac956e0a85887834bd7bbb777c1
anjana55lelum3 <amarasekaralelumamarasekara@gmail.com>
2025-06-15 17:22:36 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,6 +21,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",// session id 💙
-HACKER : process.env.HACKER || "94704638406" //Addmin number 💙
+SESSION_ID: process.env.SESSION_ID || "S0hGmb5Y#_628RmLtV12nfjGJGgpw7veNEa2COPf72XB-Nbsv_RA",// session id 💙
+HACKER : process.env.HACKER || "94726884134" //Addmin number 💙
 };
